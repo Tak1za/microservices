2ab63552839febdbc6a875e3fda43d3f3df16521
Varun Gupta <Varun.Gupta3@honeywell.com>
2019-12-30 15:06:11 +0530

    Created endpoints for accessing data

--- listings-service/src/server/startServer.js
+++ listings-service/src/server/startServer.js
@@ -2,6 +2,7 @@ import bodyParser from "body-parser";
 import cors from "cors";
 import express from "express";
 import accessEnv from "#root/helpers/accessEnv";
+import setupRoutes from './routes';
 
 const PORT = accessEnv("PORT", 7100);
 
@@ -15,6 +16,8 @@ app.use(
     credentials: true
   })
 );
+
+setupRoutes(app);
 
 app.listen(PORT, "0.0.0.0", () => {
     console.info(`Listings service listening on ${PORT}`);
