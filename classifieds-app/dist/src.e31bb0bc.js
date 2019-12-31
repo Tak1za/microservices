99a3818b2a5de05bdc54f809d9339437e823ccda
Tak1za@Ubuntu <varungupta2015135@gmail.com>
2019-12-31 23:59:26 +0530

    added classifieds-app as a microservice in docker

--- classifieds-app/dist/src.e31bb0bc.js
+++ classifieds-app/dist/src.e31bb0bc.js
@@ -36045,7 +36045,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61683" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "43687" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
